9d6349ea618ae1e670ded98a2620f58aafb3513b
Dj3ma <knaz25@mail.ru>
2018-11-07 21:30:34 +0300

    Test commit

---
+++ Test/src/ru/smirnov/Main.java
@@ -0,0 +1,13 @@
+package ru.smirnov;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    final String hello = "Hello";
+	    Scanner scan = new Scanner(System.in);
+	    String name = scan.next();
+        System.out.println(hello + " " + name);
+    }
+}
